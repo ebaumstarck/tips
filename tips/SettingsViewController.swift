58c3654dc6dd73761da5c01663a1fb99caea6147
emma baumstarck <emma.baumstarck@airbnb.com>
2016-04-29 10:35:38 -0700

    code path tips app

--- tips/SettingsViewController.swift
+++ tips/SettingsViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var switchPercent5: UISwitch!
+    @IBOutlet weak var switchPercent10: UISwitch!
+    @IBOutlet weak var switchPercent15: UISwitch!
+    @IBOutlet weak var switchPercent20: UISwitch!
+    @IBOutlet weak var switchPercent25: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +27,16 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // update switches
+        let defaults = NSUserDefaults.standardUserDefaults()
+        switchPercent5.setOn(defaults.boolForKey("5"), animated: true)
+        switchPercent10.setOn(defaults.boolForKey("10"), animated: true)
+        switchPercent15.setOn(defaults.boolForKey("15"), animated: true)
+        switchPercent20.setOn(defaults.boolForKey("20"), animated: true)
+        switchPercent25.setOn(defaults.boolForKey("25"), animated: true)
+    }
 
     /*
     // MARK: - Navigation
@@ -31,5 +47,29 @@ class SettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func updateToggle(sender: AnyObject) {
+        var percent:Int
+        if sender.isEqual(switchPercent5) {
+            percent = 5
+        } else if sender.isEqual(switchPercent10) {
+            percent = 10
+        } else if sender.isEqual(switchPercent15) {
+            percent = 15
+        } else if sender.isEqual(switchPercent20) {
+            percent = 20
+        } else if sender.isEqual(switchPercent25) {
+            percent = 25
+        } else {
+            percent = -1
+        }
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setBool(
+//            ((sender as! UISwitch).valueForKey("important")?.boolValue)!,
+            (sender as! UISwitch).on,
+            forKey: String(format:"%d", percent))
+        defaults.synchronize()
+    }
 
 }
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSelectorSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.text = ""
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tipSelectorSegment.removeAllSegments()
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        var numAdded = 0
+        var percent = 5
+        while percent <= 25 {
+            if defaults.boolForKey("\(percent)") {
+                tipSelectorSegment.insertSegmentWithTitle(
+                    "\(percent)%", atIndex: numAdded, animated: true)
+                numAdded += 1
+            }
+            percent += 5
+        }
+        tipSelectorSegment.selectedSegmentIndex = numAdded / 2
+        billField.text = ""
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+//        let tipPercentages = [0.15, 0.2, 0.25]
+//        let tipPercentage = tipPercentages[tipSelectorSegment.selectedSegmentIndex]
+        let displayedPercentage = tipSelectorSegment.titleForSegmentAtIndex(tipSelectorSegment.selectedSegmentIndex)
+        let percentage = Int(
+            (displayedPercentage?.substringToIndex(
+                (displayedPercentage?.endIndex.advancedBy(-1))!))!)
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * 0.01 * Double(percentage!)
+        let total = billAmount + tip
+//    tipSelectorSegment.insertSegmentWithTitle("FOO", atIndex: 3, animated: true)
+//        tipSelectorSegment.removese
+        
+        tipLabel.text = String(format: "$%.02f", tip)
+        totalLabel.text = String(format: "$%.02f", total)
+    }
+    
+    @IBAction func onBackgroundTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
